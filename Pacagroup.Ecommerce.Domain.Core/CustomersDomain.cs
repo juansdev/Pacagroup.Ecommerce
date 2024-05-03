bffd33d376d2e0d0cdf97b3ac2d31bd58eb19f59
JuanSDev <juansdev72@gmail.com>
2024-05-02 19:36:54 -0500

    feat: Capa de Dominio

---
+++ Pacagroup.Ecommerce.Domain.Core/CustomersDomain.cs
@@ -0,0 +1,71 @@
+﻿using Pacagroup.Ecommerce.Domain.Entity;
+using Pacagroup.Ecommerce.Domain.Interface;
+using Pacagroup.Ecommerce.Infrastructure.Interface;
+
+namespace Pacagroup.Ecommerce.Domain.Core
+{
+    public class CustomersDomain : ICustomersDomain
+    {
+        private readonly ICustomersRepository _customersRepository;
+        public CustomersDomain(ICustomersRepository customersRepository)
+        {
+            _customersRepository = customersRepository;
+        }
+        #region Sync Methods
+        public bool Delete(string customerId)
+        {
+            return _customersRepository.Delete(customerId);
+        }
+
+        public Customers Get(string customerId)
+        {
+            return _customersRepository.Get(customerId);
+        }
+
+        public IEnumerable<Customers> GetAll()
+        {
+            return _customersRepository.GetAll();
+        }
+
+        public bool Insert(Customers customer)
+        {
+            return _customersRepository.Insert(customer);
+        }
+
+        public bool Update(Customers customer)
+        {
+            return _customersRepository.Update(customer);
+        }
+
+        #endregion
+
+        #region Async Methods
+
+        public Task<bool> DeleteAsync(string customerId)
+        {
+            return _customersRepository.DeleteAsync(customerId);
+        }
+
+        public Task<IEnumerable<Customers>> GetAllAsync()
+        {
+            return _customersRepository.GetAllAsync();
+        }
+
+        public Task<Customers> GetAsync(string customerId)
+        {
+            return _customersRepository.GetAsync(customerId);
+        }
+
+        public Task<bool> InsertAsync(Customers customer)
+        {
+            return _customersRepository.InsertAsync(customer);
+        }
+
+        public Task<bool> UpdateAsync(Customers customer)
+        {
+            return _customersRepository.UpdateAsync(customer);
+        }
+
+        #endregion
+    }
+}
---
+++ Pacagroup.Ecommerce.Domain.Interface/ICustomersDomain.cs
@@ -0,0 +1,27 @@
+﻿using Pacagroup.Ecommerce.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Domain.Interface
+{
+    public interface ICustomersDomain
+    {
+        #region Sync Methods
+        bool Insert(Customers customer);
+        bool Update(Customers customer);
+        bool Delete(String customerId);
+        Customers Get(String customerId);
+        IEnumerable<Customers> GetAll();
+        #endregion
+        #region Async Methods
+        Task<bool> InsertAsync(Customers customer);
+        Task<bool> UpdateAsync(Customers customer);
+        Task<bool> DeleteAsync(String customerId);
+        Task<Customers> GetAsync(String customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+        #endregion
+    }
+}
