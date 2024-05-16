b52ec2fbea494a614fede63925e0e6c262cf4549
JuanSDev <juansdev72@gmail.com>
2024-05-16 18:39:33 -0500

    refactor: Refactorizar Program y Startup

--- Pacagroup.Ecommerce.Services.WebApi/Program.cs
+++ Pacagroup.Ecommerce.Services.WebApi/Program.cs
@@ -1,8 +1,57 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using Pacagroup.Ecommerce.Services.WebApi;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Authentication;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Feature;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.HealthCheck;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Injection;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Mapper;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Swagger;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Validator;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddMapper();
+builder.Services.AddFeature(builder.Configuration);
+builder.Services.AddInjection(builder.Configuration);
+builder.Services.AddAuthentications(builder.Configuration);
+builder.Services.AddVersioning();
+builder.Services.AddSwagger();
+builder.Services.AddValidator();
+builder.Services.AddHealthCheck(builder.Configuration);
+
 var app = builder.Build();
-startup.Configure(app, app.Environment, app.Services.GetRequiredService<IApiVersionDescriptionProvider>());
+
+// Configure the HTTP Request Pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        // Build a swagger endpoint for each discovered API version
+        var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API Ecommerce V1");
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+        }
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseCors("policyApiEcommerce");
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.MapHealthChecksUI();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.Run();
