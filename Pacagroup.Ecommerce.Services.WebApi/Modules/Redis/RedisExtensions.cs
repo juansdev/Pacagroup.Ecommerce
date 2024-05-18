83e88621ad76c0fa80df78388dcb1fb5eaf4c4ad
JuanSDev <juansdev72@gmail.com>
2024-05-18 17:35:46 -0500

    feat: Redis

---
+++ Pacagroup.Ecommerce.Services.WebApi/Modules/Redis/RedisExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Redis
+{
+    public static class RedisExtensions
+    {
+        public static IServiceCollection AddRedisCache(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration.GetConnectionString("RedisConnection");
+            });
+            return services;
+        }
+    }
+}
