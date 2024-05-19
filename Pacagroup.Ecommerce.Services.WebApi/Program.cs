b5c6c83691db5afec879f002162e758ddb21a6b9
JuanSDev <juansdev72@gmail.com>
2024-05-19 15:03:38 -0500

    feat: Arquitectura limpia capa de infrastructura

--- Pacagroup.Ecommerce.Services.WebApi/Program.cs
+++ Pacagroup.Ecommerce.Services.WebApi/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddMapper();
 builder.Services.AddFeature(builder.Configuration);
-builder.Services.AddPersistenceServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddApplicationServices();
 builder.Services.AddInjection(builder.Configuration);
 builder.Services.AddAuthentications(builder.Configuration);
